43c8d572a6a8de65c6735292f4ad63048926ca79
soldsoul <soldsoul@users.noreply.github.com>
2018-08-27 16:31:02 -0700

    Update index.js

--- src/html/get-index/index.js
+++ src/html/get-index/index.js
@@ -13,7 +13,7 @@ let defaultHTML = `
 
     <h1 class="center-text">
       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      Hey there!
     </h1>
 
     <p class="center-text">
